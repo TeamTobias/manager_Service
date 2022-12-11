92a341bd00f650c8dc260110ea30aa3e9ea0b238
DietCoding <jaejoodo1@gmail.com>
2022-12-11 13:43:06 +0900

    카프카컨슈머 로직추가

--- src/main/java/com/tobias/managerservice/outer/adaptor/KafkaConsumer.java
+++ src/main/java/com/tobias/managerservice/outer/adaptor/KafkaConsumer.java
@@ -5,21 +5,23 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tobias.managerservice.inner.domain.SaleRequest;
 import com.tobias.managerservice.inner.repository.ManagerRepository;
+import com.tobias.managerservice.inner.repository.SaleRequestRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 @Slf4j
 public class KafkaConsumer {
-    ManagerRepository repository;
+    SaleRequestRepository repository;
 
     @Autowired
-    public KafkaConsumer(ManagerRepository repository){
+    public KafkaConsumer(SaleRequestRepository repository){
         this.repository = repository;
     }
 
@@ -35,7 +37,15 @@ public class KafkaConsumer {
             e.printStackTrace();
         }
 
-
-        //repository.save(entity);
+        SaleRequest saleRequest = new SaleRequest();
+        saleRequest.setSalerId((int)map.get("id"));
+        saleRequest.setName((String)map.get("name"));
+        saleRequest.setNickname((String)map.get("nickname"));
+        saleRequest.setEmail((String)map.get("email"));
+        saleRequest.setGender((String)map.get("gender"));
+        saleRequest.setBirth((String)map.get("birth"));
+        saleRequest.setPhone((String)map.get("phone"));
+        saleRequest.setWebsite((String)map.get("website"));
+        repository.save(saleRequest);
     }
 }
